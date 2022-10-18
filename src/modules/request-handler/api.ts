6ea9ecbd925cf34c3dd076191098314d81348ad2
Lennard Claproth <lennard@serviceheroes.com>
2022-10-18 21:36:31 +0200

    environment variable fix

--- src/modules/request-handler/api.ts
+++ src/modules/request-handler/api.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 import https from 'https';
 
-const baseURL = process.env.BASE_URL;
+const baseURL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const api = axios.create({
   baseURL,
